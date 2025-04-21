79797a993716bf0c48f9d6c594ccf268f920045e
urdeath999 <cedrixjames555@gmail.com>
2025-04-21 19:57:04 +0800

    Changes the token

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -46,8 +46,10 @@ export const register = async (req, res) => {
         //Cookie store token in browser
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+            // secure: process.env.NODE_ENV === 'production',
+            secure: true,
+            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -115,8 +117,10 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+            // secure: process.env.NODE_ENV === 'production',
+            secure: true,
+            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
@@ -140,8 +144,10 @@ export const logout = async (req, res) => {
         //Clear cookie
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+            // secure: process.env.NODE_ENV === 'production',
+            secure: true,
+            // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',  
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
